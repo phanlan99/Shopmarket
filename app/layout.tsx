9ba28384ece5d76df5efdee16c44700e43431fd2
phanlan99 <youremail@example.com>
2025-09-15 01:36:06 +0700

    theme toggle

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import '@/assets/styles/globals.css';
-import { APP_NAME , APP_DESCRIPTION , SERVER_URL } from '@/lib/constant';
+import { APP_NAME, APP_DESCRIPTION, SERVER_URL } from '@/lib/constant';
+import { ThemeProvider } from 'next-themes';
+
+
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -21,7 +24,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en' suppressHydrationWarning>
-      <body className={`${inter.className}`}>{children}</body>
+      <body className={`${inter.className}`}>
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='light'
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider></body>
     </html>
   );
 }
