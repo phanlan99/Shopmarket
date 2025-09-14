09797b721f82731c254f1f9ae1be6d3ac36db666
phanlan99 <youremail@example.com>
2025-09-15 00:56:31 +0700

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <Button>abcd</Button>
+    </div>
+  )
+}
+
+export default page
